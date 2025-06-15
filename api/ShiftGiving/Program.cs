f2d3968dc7d31b053d90ad351d5718e0dc984156
Travis Smith <travis.smith@shiftdsm.com>
2025-06-15 01:05:00 -0500

    Deploying api and web apps to aws.

--- api/ShiftGiving/Program.cs
+++ api/ShiftGiving/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecks();
-builder.WebHost.UseUrls("http://*:80");
+if (builder.Environment.IsProduction())
+{
+    builder.WebHost.UseUrls("http://*:80");
+}
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
