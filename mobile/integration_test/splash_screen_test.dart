735bfd57e298ccc95e5045fc438e52035e073188
Travis Smith <travis.smith@shiftdsm.com>
2025-07-21 16:17:06 -0500

    Updating workflow to fix ios test.

--- mobile/integration_test/splash_screen_test.dart
+++ mobile/integration_test/splash_screen_test.dart
@@ -1,25 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:flutter/material.dart';
 
-import 'app.dart';
+import 'package:shift_giving/main.dart' as app;
 import 'package:shift_giving/home_page.dart';
 import 'package:shift_giving/splash_screen.dart';
 
 void splashScreenTests() {
   group('Splash Screen Navigation Tests', () {
     testWidgets(
-      'Should navigate from SplashScreen to HomePage quickly in test mode',
+      'Should navigate from SplashScreen to HomePage after delay',
           (WidgetTester tester) async {
-        await tester.pumpWidget(
-          const ShiftGivingApp(splashDelay: Duration(milliseconds: 100))
-        );
+        app.main();
         await tester.pumpAndSettle();
 
         expect(find.byType(SplashScreen), findsOneWidget);
         expect(find.byType(HomePage), findsNothing);
 
-        await tester.pump(const Duration(milliseconds: 150));
+        await Future.delayed(const Duration(seconds: 4));
         await tester.pumpAndSettle();
 
         expect(find.byType(SplashScreen), findsNothing);
